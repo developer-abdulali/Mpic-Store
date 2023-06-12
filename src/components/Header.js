409e3fe39e0b486c0c7d3560229c9ef8944babdf
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-12 17:00:11 +0500

    added changes

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='sticky top-0 bg-white z-10 shadow'>
+        <div className='container mx-auto p-6 flex justify-between'></div>
+
+    </header>
+  )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,17 @@
-import { Inter } from "next/font/google"
+import { Inter } from "next/font/google";
+import Header from "src/components/Header";
 
 const inter = Inter({
-    subsets: ['latin'],
-    variable: '--font-inter'
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
-export default function AppLayout({children}) {
+export default function AppLayout({ children }) {
   return (
     <div className={`${inter.variable} font-sans min-h-screen flex flex-col`}>
-        <header>header</header>
-    <main className='flex-grow bg-[#f7f7f7]'>{children}</main>
-    <footer>footer</footer>
+      <Header />
+      <main className="flex-grow bg-[#f7f7f7]">{children}</main>
+      <footer>footer</footer>
     </div>
-  )
+  );
 }
- 
