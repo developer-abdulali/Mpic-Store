26001dba3e21686a07774b7cec894f815dc5493d
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-12 20:00:06 +0500

    completed navbar and footer

---
+++ seed.js
@@ -0,0 +1,78 @@
+// const Stripe = require('stripe')
+// const Products = require('./products')
+
+// const stripe = Stripe('sk_test_51NIAyqDYMoQlsKb0hrsJ6ewNV81FRCyJ8VwE1NEDKbujpOq79kgRomTw3zOUkZcUiBgTxeA9qiu5e8Yz0XMpTCtv003ub5bTmB')
+
+// (async () => {
+//     for (const product of products){
+//         const stripeProduct = await stripe.products.create({
+//             name: product.name,
+//             default_price_data: {
+//                 currency: product.currency,
+//                 unit_amount_decimal: product.price,
+//             },
+//             images: [product.image],
+//         }),
+//         console.log(stripeProduct.name,":",stripeProduct.id);
+//     }
+// })();
+
+
+
+
+
+
+// const Stripe = require('stripe');
+// const Products = require('./products');
+
+// const stripe = Stripe('sk_test_51NIAyqDYMoQlsKb0hrsJ6ewNV81FRCyJ8VwE1NEDKbujpOq79kgRomTw3zOUkZcUiBgTxeA9qiu5e8Yz0XMpTCtv003ub5bTmB');
+
+// (async () => {
+//     const products = Products.products;
+
+//     for (const product of products) {
+//         const stripeProduct = await stripe.products.create({
+//             name: product.name,
+//             default_price_data: {
+//                 currency: product.currency,
+//                 unit_amount_decimal: product.price,
+//             },
+//             images: [product.image],
+//         });
+
+//         console.log(stripeProduct.name, ":", stripeProduct.id);
+//     }
+// })();
+
+
+
+
+
+
+
+
+
+const Stripe = require("stripe");
+const Products = require("./products");
+
+
+
+const stripe = Stripe('sk_test_51NIAyqDYMoQlsKb0hrsJ6ewNV81FRCyJ8VwE1NEDKbujpOq79kgRomTw3zOUkZcUiBgTxeA9qiu5e8Yz0XMpTCtv003ub5bTmB');
+
+
+(async () => {
+    const products = Products.products;
+
+    for (const product of products) {
+        const stripeProduct = await stripe.products.create({
+            name: product.name,
+            default_price_data: {
+                currency: product.currency,
+                unit_amount_decimal: product.price,
+            },
+            images: [product.image],
+        });
+
+        console.log(stripeProduct.name, ":", stripeProduct.id);
+    }
+})();
