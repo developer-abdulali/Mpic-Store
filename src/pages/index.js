3ee75b7d8586f315786b7b3e2e09b989ae8270a1
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-13 01:18:32 +0500

    added api data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
 import { Inter } from 'next/font/google'
-
+import { stripe } from 'src/utils/stripe'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({products}) {
+  // console.log(products)
   return (
-    <h1 className=''>hi</h1>
+    <div className=''>home</div>
   )
+}
+
+export async function getStaticProps(){
+
+  const inventory = await stripe.products.list({
+    limit: 8,
+  })
+  return{
+    props:{
+      products: inventory
+    }
+  }
 }
