39aba5556f2a21fb45b6642c9decc6ebd39f6c65
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-13 16:09:09 +0500

    added changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,33 @@
- import React from 'react'
-import { stripe } from 'src/utils/stripe'
- 
- export default function index({products}) {
-  console.log(products)
+import { stripe } from "src/utils/stripe";
 
-   return (
-     <div>home</div>
-   )
- }
- 
-export async function getStaticProps(){
+export default function index({ products }) {
+  console.log(products);
+
+  return <div>home</div>;
+}
+
+export async function getStaticProps() {
   const inventory = await stripe.products.list({
     expand: ["data.default_price"],
     limit: 8,
-  })
-  const products = inventory.data.map(product => {
-    const price = product.default_price
-    return{
+  });
+
+  const products = inventory.data.map((product) => {
+    const price = product.default_price;
+    const image = product.images.length > 0 ? product.images[0] : null;
+
+    return {
       currency: price.currency,
       id: product.id,
       name: product.name,
       price: price.unit_amount,
-      image: product.images[0]
-    }
-  })
-  return{
-    props:{
+      image: image,
+    };
+  });
+
+  return {
+    props: {
       products,
-    }
-  }
+    },
+  };
 }
