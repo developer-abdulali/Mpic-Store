fd50977e299971d616829ea814eb779cab09c76b
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-14 00:37:31 +0500

    added changes in Ratings components

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['files.stripe.com']
-  },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['files.stripe.com']
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+
+module.exports = {
+  images: {
+    reactStrictMode: true,
+    domains: ['files.stripe.com'], 
+  },
+};
--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -5,7 +5,7 @@ export default function Ratings() {
   return (
     <div className="flex items-center -ml-1">
         {Array.from({ length: 4 }).map((_, index)=>(
-            <StarIcon key={index} className="w-6 h-6 flex flex-shrink-0 text-yellow-600" />
+            <StarIcon key={index} className="w-6 h-6 flex flex-shrink-0 text-yellow-400" />
         ))}
 
     </div>
