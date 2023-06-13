87d85486aa5b0ae1aa921a44d36e7b6238764463
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-13 23:41:14 +0500

    added images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   domains: ['epic-projects.nyc3.digitaloceanspaces.com']
-  // },
+  images: {
+    domains: ['files.stripe.com']
+  },
 }
 
 module.exports = nextConfig
