152d79d48fd9ddb9e9c09806cf3a57a0436ce108
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-15 02:09:31 +0500

    added changes

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,23 +1,24 @@
 import { stripe } from "src/utils/stripe";
 import Image from "next/image";
-import { CheckIcon } from "@heroicons/react/24/solid";
-export default function ProductPage({ products }) {
-  console.log(products);
+import { CheckIcon } from "@heroicons/react/24/outline";
+
+export default function ProductPage({ product }) {
+  console.log(product);
   return (
     <div className="container lg:max-w-screen-lg mx-auto py-12 px-6">
       <div className="flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:space-x-12">
         <div className="relative w-72 h-72 sm:w-96 sm:h-96">
-          <Image
-            src={products.image}
-            alt={products.name}
-            fill
-            style={{ objectFit: "contain" }}
-            sizes="100%"
-            priority
-          />
+          {product && product.image && (
+            <Image
+              src={product.image}
+              alt={product.name}
+              layout="fill"
+              objectFit="contain"
+            />
+          )}
         </div>
         <div className="w-full flex-1 max-w-md border border-opacity-50 rounded-md shadow-lg p-6 bg-white">
-          <h2 className="text-3xl font-semibold">{products.name}</h2>
+          <h2 className="text-3xl font-semibold">{product.name}</h2>
           <p className="pt-2 flex items-center space-x-2">
             <CheckIcon className="text-lime-500" />
           </p>
@@ -32,7 +33,6 @@ export async function getStaticPaths() {
   const paths = inventory.data.map((product) => ({
     params: { id: product.id },
   }));
-
   return {
     paths,
     fallback: "blocking",
@@ -46,21 +46,20 @@ export async function getStaticProps({ params }) {
 
   const products = inventory.data.map((product) => {
     const price = product.default_price;
-    const image = product.images.length > 0 ? product.images[0] : null;
-
     return {
       currency: price.currency,
       id: product.id,
       name: product.name,
       price: price.unit_amount,
-      image: image,
+      image: product.images.length > 0 ? product.images[0] : null,
     };
   });
+
   const product = products.find((product) => product.id === params.id);
 
   return {
     props: {
-      products,
+      product,
     },
     revalidate: 60 * 60,
   };
