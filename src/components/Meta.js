b93fc5c2626822379818e7aa095a3a2212129474
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-17 02:23:10 +0500

    added changes

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -3,7 +3,7 @@ import Head from "next/head"
 export default function Meta() {
   return (
     <Head>
-      <title>EpicStore</title>
+      <title>MpicStore</title>
       <link rel="icon" type="image/x-icon' href='/favicon.ico?v=1" />
     </Head>
   )
