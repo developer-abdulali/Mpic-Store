2cfc18bb1b340bb1daf8f88a7a52fe530c657871
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-16 19:24:40 +0500

    added changes

---
+++ src/pages/api/checkout-sessions/[id].js
@@ -0,0 +1,24 @@
+import { stripe } from "src/utils/stripe";
+
+import React from 'react'
+
+export default async function handler(req, res) {
+    if(req.method === "POST"){
+    const id = req.query.id
+    try {
+        if(!id.startsWith("cs_")){
+            throw new Error("Incorrect checkout session id")
+            res.status(200).json(checkoutSession)
+        }
+        const checkoutSession = await stripe.checkout.sessions.retrieve(id)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({statusCode: 500, message: error.message})
+    }} else{
+        res.SetHeader("Allow" , "GET")
+        res.status(405).end("Method Not Allowed")
+    }
+
+
+}
